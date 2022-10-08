2d275595c5370f8af699a1a8c8dafa7020a8eb9f
ssk-gh <78728203+ssk-gh@users.noreply.github.com>
2022-10-08 04:40:41 +0000

    Add comments to code-generator

--- code-generator.mjs
+++ code-generator.mjs
@@ -2,22 +2,29 @@ import fs from 'fs'
 import path from 'path'
 import glob from 'glob'
 
+// Generate files to provide paths for Redirect
+
 const getCode = (parentCount) => {
     const parentPath = [...Array(parentCount)].map((x) => '..').join('/')
-    return `import { Redirect } from '${parentPath}/components/redirect'
+    return `// This is the generated code for Redirect. See code-generator.mjs
+import { Redirect } from '${parentPath}/components/redirect'
 export default Redirect`
 }
 
-const pages = glob.sync('pages/\\[locale\\]/**/*.tsx')
+const generateCodes = () => {
+    const pages = glob.sync('pages/\\[locale\\]/**/*.tsx')
 
-pages.forEach((page) => {
-    const parentCount = page.split('/').length - 2
-    const code = getCode(parentCount)
-    const dest = page.replace('/[locale]', '')
-    const dirname = path.dirname(dest)
+    pages.forEach((page) => {
+        const parentCount = page.split('/').length - 2
+        const code = getCode(parentCount)
+        const dest = page.replace('/[locale]', '')
+        const dirname = path.dirname(dest)
 
-    if (!fs.existsSync(dirname)) {
-        fs.mkdirSync(dirname, { recursive: true })
-    }
-    fs.writeFileSync(dest, code, { encoding: 'utf8' })
-})
+        if (!fs.existsSync(dirname)) {
+            fs.mkdirSync(dirname, { recursive: true })
+        }
+        fs.writeFileSync(dest, code, { encoding: 'utf8' })
+    })
+}
+
+generateCodes()
--- pages/docs/[slug].tsx
+++ pages/docs/[slug].tsx
@@ -1,2 +1,3 @@
+// This is the generated code for Redirect. See code-generator.mjs
 import { Redirect } from '../../components/redirect'
 export default Redirect
